148f30b13f1e7a0abbe664539a96131baaf249ea
ayonij2006 <ayonij2006@gmail.com>
2025-05-31 11:49:11 +0545

    added endpoint for file upload and python-multipart package

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    file_size = len(contents)
+    
+    return JSONResponse({
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "file_size_bytes": file_size
+    })
